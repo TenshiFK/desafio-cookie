d20e672b7185e1aa7fd3aab3504e4957d3bc3d27
Bowie <flavia.kredenser14@gmail.com>
2023-11-12 21:24:44 -0300

    commit

--- desafio.js
+++ desafio.js
@@ -17,7 +17,7 @@ app.use(
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.render('index');
 });
 
 app.get('/cookie', (req, res) => {
